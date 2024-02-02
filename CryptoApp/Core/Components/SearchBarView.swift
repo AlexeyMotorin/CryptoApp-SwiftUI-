3f52c42445bf6549836f8bb371747dd0f024b67a
AlexeyMotorin <aleksey.motorin2015@yandex.ru>
2024-02-02 14:35:46 +0300

    added SearchBarView

---
+++ CryptoApp/Core/Components/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  CryptoApp
+//
+//  Created by Алексей Моторин on 02.02.2024.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
---
+++ CryptoApp/Extensions/UIApplication+extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIApplication+extension.swift
+//  CryptoApp
+//
+//  Created by Алексей Моторин on 02.02.2024.
+//
+
+import Foundation
