d819a8a2c9ccb686baca57e2a310e58e8e9375ab
AlexeyMotorin <aleksey.motorin2015@yandex.ru>
2024-02-08 13:12:27 +0300

    added MarketDataService

---
+++ CryptoApp/Core/Services/MarketDataService.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDataService.swift
+//  CryptoApp
+//
+//  Created by Алексей Моторин on 07.02.2024.
+//
+
+import Foundation
---
+++ CryptoApp/Models/MarketDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDataModel.swift
+//  CryptoApp
+//
+//  Created by Алексей Моторин on 07.02.2024.
+//
+
+import Foundation
